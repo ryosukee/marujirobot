590a20ace53b45335bb990caf5323148e9fe2490
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-20 01:17:04 +0900

    [add] reply

--- api.py
+++ api.py
@@ -13,11 +13,43 @@ class API:
         self.auth = OAuth1(CK, CS, AT, AS)
         self.url = 'https://api.twitter.com/1.1/statuses/'
 
+        self.last_id = None
+
+    def __post(self, url, params=None):
+        if params is None:
+            response = requests.post(url, auth=self.auth)
+        else:
+            response = requests.post(url, auth=self.auth, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def __get(self, url, params=None):
+        if params is None:
+            response = requests.get(url, auth=self.auth)
+        else:
+            response = requests.get(url, auth=self.auth, params=params)
+        response.raise_for_status()
+        return response.json()
+
     def tweet(self, message):
         url = self.url + 'update.json'
         params = {'status': message}
-        req = requests.post(url, auth=self.auth, params=params)
-        if req.status_code == 200:
-            print("OK")
+        self.__post(url, params)
+
+    def reply(self, message, reply_id, reply_name):
+        url = self.url + 'update.json'
+        message = '@{} {}'.format(reply_name, message)
+        params = {'status': message, 'in_reply_to_status_id': reply_id}
+        self.__post(url, params)
+
+    def get_mentions(self):
+        url = self.url + 'mentions_timeline.json'
+        if self.last_id is None:
+            mentions = self.__get(url)
         else:
-            print("Error: %d" % req.status_code)
+            params = {'since_id': self.last_id}
+            mentions = self.__get(url, params)
+
+        for mention in mentions:
+            yield mention
+            self.last_id = mention['id_str']
--- example.py
+++ example.py
@@ -1,4 +1,18 @@
 from api import API
 
 api = API()
-api.tweet('tweet test2')
+# ツイートする
+# api.tweet('tweet test2')
+
+# リプを取得
+for mention in api.get_mentions():
+    text = mention['text']
+    tweet_id = mention['id_str']
+    screen_name = mention['user']['screen_name']
+    # ユーザ情報(プロフィール)
+    name = mention['user']['name']
+    zone = mention['user']['time_zone']
+    location = mention['user']['location']
+    description = mention['user']['description']
+    # リプを返す
+    api.reply(' '.join(text.split()[1:]), tweet_id, screen_name)
