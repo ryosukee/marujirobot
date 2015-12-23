dc3483d240dbe50adf21851705e331a1edce2239
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-24 03:20:06 +0900

    [add] terminal mode

--- marujirobot.py
+++ marujirobot.py
@@ -1,27 +1,30 @@
+"""
+Usage:
+    marujirobot.py [--terminal]
+
+Options:
+    -h, --help
+        show this message
+    -v, --version
+        show version
+    --terminal
+        run on terminal
+"""
+
+
+import sys
+from docopt import docopt
 from api import API
 from utils import Utils
 
 
 with API() as api:
     utils = Utils()
-    # ツイートする
-    # api.tweet('tweet test2')
-    # リプを取得
-    for mention in api.get_mentions():
-        text = mention['text']
-        tweet_id = mention['id_str']
-        screen_name = mention['user']['screen_name']
-        # ユーザ情報(プロフィール)
-        name = mention['user']['name']
-        zone = mention['user']['time_zone']
-        location = mention['user']['location']
-        description = mention['user']['description']
+    args = docopt(__doc__)
 
-        # echo
-        content = ' '.join(text.split()[1:])
-
-        # weather
-        if content == '天気':
+    # run on terminal
+    if args['--terminal']:
+        for line in iter(sys.stdin.readline, '\n'):
             # jsonの情報の取得
             wethers = utils.get_wether()
             # 整理
@@ -37,5 +40,23 @@ with API() as api:
             if maxt is not None:
                 content += '最高気温は{}℃, 最低気温は{}℃です．\n'.format(
                     maxt['celsius'], mint['celsius'])
-        # リプを返す
-        api.reply(content, tweet_id, screen_name)
+            print(content)
+    else:
+        # ツイートする
+        # api.tweet('tweet test2')
+        # リプを取得
+        for mention in api.get_mentions():
+            text = mention['text']
+            tweet_id = mention['id_str']
+            screen_name = mention['user']['screen_name']
+            # ユーザ情報(プロフィール)
+            name = mention['user']['name']
+            zone = mention['user']['time_zone']
+            location = mention['user']['location']
+            description = mention['user']['description']
+
+            # echo
+            content = ' '.join(text.split()[1:])
+
+            # リプを返す
+            api.reply(content, tweet_id, screen_name)
