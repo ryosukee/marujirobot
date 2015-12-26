3402711b29faab0e52b5cd16a4751b8502891eb3
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-26 17:13:31 +0900

    [add] morph parser

--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
 import requests
+from janome.tokenizer import Tokenizer
 
 
 class Utils:
     def __init__(self):
-        pass
+        self.__tokenizer = Tokenizer()
 
     def get_wether(self):
         url = 'http://weather.livedoor.com/forecast/webservice/json/v1'
         tokyo = '130010'
         params = {'city': tokyo}
         return requests.get(url, params=params).json()['forecasts']
+
+    def morph_parse(self, text):
+        for tok in self.__tokenizer.tokenize(text):
+            yield tok
