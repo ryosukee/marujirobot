8611d3d10cd93cbb514f887425608d022801504b
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-27 01:03:16 +0900

    [add] some comments

--- marujirobot.py
+++ marujirobot.py
@@ -26,16 +26,19 @@ with TwitterAPI() as twiapi:
     agent = DialogueAgent(args['--terminal'], twiapi)
 
     if args['--weather']:
-        # weatherの取得
+        # 今日のweatherの取得
         text = agent.weather_text()
         # ツイートする
         agent.speech('@marujiruo {}'.format(text))
     elif args['--lang8']:
+        # 今日のlang8投稿してるかどうか
         text = agent.lang8_text()
+        # textが空なら投稿済み
         if text != '':
+            # 投稿してなかったらリマインダーツイート
             agent.speech('@marujiruo {}'.format(text))
     else:
-        # 返事をする
+        # リプライに対して返事をする
         for message, tweet_id, screen_name in agent.get_message():
             text = agent.generate_reply(message)
             agent.speech(text, tweet_id, screen_name)
