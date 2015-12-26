d82dce45ade4f983372e929a5f981b7e2e7dd6a7
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-26 16:32:16 +0900

    [add] use dialogue_agent

--- marujirobot.py
+++ marujirobot.py
@@ -14,55 +14,20 @@ Options:
 """
 
 
-import sys
 from docopt import docopt
-from api import API
-from utils import Utils
+from twitter_api import TwitterAPI
+from dialogue_agent import DialogueAgent
 
 
-with API() as api:
-    utils = Utils()
+with TwitterAPI() as twiapi:
     args = docopt(__doc__)
+    agent = DialogueAgent(args['--terminal'], twiapi)
 
-    # run on terminal
-    if args['--terminal']:
-        for line in iter(sys.stdin.readline, '\n'):
-            print(line.strip())
-    elif args['--weather']:
-        # jsonの情報の取得
-        wethers = utils.get_wether()
-        # 整理 today
-        day = 0
-        date = wethers[day]['date']
-        date_label = wethers[day]['dateLabel']
-        date = '{}月{}日'.format(date.split('-')[1], date.split('-')[2])
-        telop = wethers[day]['telop']
-        maxt = wethers[day]['temperature']['max']
-        mint = wethers[day]['temperature']['min']
-
-        # テキストの生成
-        content = '{}({})の天気は{}'.format(date_label, date, telop)
-        if mint is not None:
-            content += ', \n最低気温は{}℃'.format(mint['celsius'])
-        if maxt is not None:
-            content += ',\n最高気温は{}℃'.format(maxt['celsius'])
-        content += 'です！\n'
+    if args['--weather']:
+        # weatherの取得
+        text = agent.get_weather()
         # ツイートする
-        api.tweet('@marujiruo {}'.format(content))
+        agent.speech('@marujiruo {}'.format(text))
     else:
-        # リプを取得
-        for mention in api.get_mentions():
-            text = mention['text']
-            tweet_id = mention['id_str']
-            screen_name = mention['user']['screen_name']
-            # ユーザ情報(プロフィール)
-            name = mention['user']['name']
-            zone = mention['user']['time_zone']
-            location = mention['user']['location']
-            description = mention['user']['description']
-
-            # echo
-            content = ' '.join(text.split()[1:])
-
-            # リプを返す
-            api.reply(content, tweet_id, screen_name)
+        # 返事をする
+        agent.reply()
