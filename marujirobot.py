96e00e7facb667ed879badc239841387cfac90da
ryosukee <tmcit.miyazaki@gmail.com>
2016-01-21 21:02:56 +0900

    [add] command reply mode

--- marujirobot.py
+++ marujirobot.py
@@ -25,31 +25,59 @@ from datamanager import DataManager
 from dialogue_agent import DialogueAgent
 
 
+def weather(screen_name='marujiruo', tweet_id=None):
+    # 今日のweatherの取得
+    text = agent.weather_text()
+    # ツイートする
+    if tweet_id is None:
+        agent.speech('@{} {}'.format(screen_name, text))
+    else:
+        agent.speech(text, tweet_id, screen_name)
+
+
+def lang8reminder(screen_name='marujiruo', tweet_id=None):
+    # 今日のlang8投稿してるかどうか
+    text = agent.lang8_reminder_text()
+    # textが空なら投稿済み
+    if text != '':
+        # 投稿してなかったらリマインダーツイート
+        if tweet_id is None:
+            agent.speech('@{} {}'.format(screen_name, text), avoid_dupl='！')
+        else:
+            agent.speech(text, tweet_id, screen_name)
+
+
+def lang8check(screen_name='marujiruo', tweet_id=None):
+    # 昨日は誰が投稿忘れたか
+    text = agent.lang8_check_text()
+    if text != '':
+        if tweet_id is None:
+            agent.speech('@{} {}'.format(screen_name, text), avoid_dupl='！')
+        else:
+            agent.speech(text, tweet_id, screen_name)
+
+
 with DataManager() as db:
     args = docopt(__doc__)
     agent = DialogueAgent(args['--terminal'], db)
 
     if args['--weather']:
-        # 今日のweatherの取得
-        text = agent.weather_text()
-        # ツイートする
-        agent.speech('@marujiruo {}'.format(text))
+        weather()
     elif args['--lang8-reminder']:
-        # 今日のlang8投稿してるかどうか
-        text = agent.lang8_reminder_text()
-        # textが空なら投稿済み
-        if text != '':
-            # 投稿してなかったらリマインダーツイート
-            agent.speech('@marujiruo {}'.format(text), avoid_dupl='！')
+        lang8reminder()
     elif args['--lang8-check']:
-        # 昨日は誰が投稿忘れたか
-        text = agent.lang8_check_text()
-        if text != '':
-            agent.speech('@marujiruo {}'.format(text), avoid_dupl='！')
+        lang8check()
     elif args['--tweet'] is not None:
         agent.speech(args['--tweet'])
     else:
         # リプライに対して返事をする
         for message, tweet_id, screen_name in agent.get_message():
-            text = agent.generate_reply(message)
-            agent.speech(text, tweet_id, screen_name)
+            if message == '--weather':
+                weather(screen_name, tweet_id)
+            elif message == '--lang8-check':
+                lang8check(screen_name, tweet_id)
+            elif message == '--lang8-reminder':
+                lang8reminder(screen_name, tweet_id)
+            else:
+                text = agent.generate_reply(message)
+                agent.speech(text, tweet_id, screen_name)
